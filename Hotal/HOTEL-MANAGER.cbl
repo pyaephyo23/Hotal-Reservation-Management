5d6ae694b08a41fd2a43cea406a83083098e66bd
Pyae Phyo Kyaw <pyaephyokyaw@ucsy.edu.mm>
2025-07-02 08:54:29 +0630

    input data file

--- Hotal/HOTEL-MANAGER.cbl
+++ Hotal/HOTEL-MANAGER.cbl
@@ -47,6 +47,7 @@
        01 BASE-AMOUNT       PIC 9(5)V99.
        01 FINAL-AMOUNT      PIC 9(5)V99.
        01 WS-LINE           PIC X(80).
+       01 INPUT-ROOM-TYPE      PIC X(10).
        01 WS-EOF            PIC X VALUE 'N'.
       *-----------------------
        PROCEDURE DIVISION.
@@ -77,13 +78,13 @@
 
            BOOK-ROOM.
            DISPLAY "Enter room type (SINGLE/DOUBLE/DELUXE): ".
-           ACCEPT ROOM-TYPE.
+           ACCEPT INPUT-ROOM-TYPE.
            OPEN INPUT ROOM-FILE.
            PERFORM UNTIL WS-EOF = 'Y'
                READ ROOM-FILE
                    AT END MOVE 'Y' TO WS-EOF
                    NOT AT END
-                       IF ROOM-TYPE = ROOM-TYPE AND
+                       IF ROOM-TYPE = INPUT-ROOM-TYPE AND
                            ROOM-STATUS = "AVAILABLE"
                            MOVE "BOOKED" TO ROOM-STATUS
                            REWRITE ROOM-RECORD
