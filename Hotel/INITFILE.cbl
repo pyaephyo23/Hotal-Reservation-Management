403b2394aab81dff63ebd93a6a1a056687bf5696
Kaung Myat Htun <kaungkaung4440.kk@gmail.com>
2025-07-10 07:31:17 +0630

    Squashed commit of the following:
    commit 916a5b4d5cfb36a4e3e9c4707fec8501b9fe16e6
    Merge: 35d3c04 ecca69e
    Author: Pyae Phyo Kyaw <pyaephyokyaw@ucsy.edu.mm>
    Date:   Tue Jul 8 10:01:00 2025 +0630
    
        Merge pull request #8 from pyaephyo23/ppk
    
        initfile customer , booking
    
    commit ecca69e921c2cfc56fc1995f7ccd9d793019a20e
    Author: Pyae Phyo Kyaw <pyaephyokyaw@ucsy.edu.mm>
    Date:   Tue Jul 8 10:00:01 2025 +0630
    
        initfile customer , booking
    
    commit 35d3c04c3bf46cd01d1c0a384205e4fb9041df9a
    Merge: 4c65471 075fa2a
    Author: Pyae Phyo Kyaw <pyaephyokyaw@ucsy.edu.mm>
    Date:   Tue Jul 8 09:53:04 2025 +0630
    
        Merge pull request #7 from pyaephyo23/ppk
    
        Create bookRoom.cbl
    
    commit 075fa2a7a8f9dc7be20c7ac26a2a85488861b728
    Author: Pyae Phyo Kyaw <pyaephyokyaw@ucsy.edu.mm>
    Date:   Tue Jul 8 09:51:02 2025 +0630
    
        Create bookRoom.cbl

---
+++ Hotel/INITFILE.cbl
@@ -0,0 +1,43 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. INITFILES.
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT CUSTOMER-FILE ASSIGN TO '../DATA/CUSTOMER.DAT'
+               ORGANIZATION IS INDEXED
+               ACCESS MODE IS DYNAMIC
+               RECORD KEY IS CUSTOMER-ID.
+           SELECT BOOKING-FILE ASSIGN TO '../DATA/BOOKING.DAT'
+               ORGANIZATION IS INDEXED
+               ACCESS MODE IS DYNAMIC
+               RECORD KEY IS BOOKING-ID.
+       DATA DIVISION.
+       FILE SECTION.
+       FD  CUSTOMER-FILE.
+       01  CUSTOMER-RECORD.
+           05 CUSTOMER-ID     PIC 9(6).
+           05 CUSTOMER-NAME   PIC X(30).
+           05 CUSTOMER-PHONE  PIC X(15).
+           05 CUSTOMER-EMAIL  PIC X(30).
+           05 CUSTOMER-ADDR   PIC X(50).
+       FD  BOOKING-FILE.
+       01  BOOKING-RECORD.
+           05 BOOKING-ID      PIC 9(6).
+           05 ROOM-ID-BK      PIC X(5).
+           05 CUSTOMER-ID-BK  PIC 9(6).
+           05 CHECKIN-DATE    PIC X(8).
+           05 CHECKOUT-DATE   PIC X(8).
+           05 BOOKING-STATUS  PIC X(10).
+       WORKING-STORAGE SECTION.
+       01 WS-DUMMY-VALUE     PIC X VALUE ' '.
+       PROCEDURE DIVISION.
+       BEGIN.
+           OPEN OUTPUT CUSTOMER-FILE
+           OPEN OUTPUT BOOKING-FILE
+
+           *> Optionally write a dummy record, or just close to create empty files
+           CLOSE CUSTOMER-FILE
+           CLOSE BOOKING-FILE
+
+           DISPLAY "Files initialized."
+           STOP RUN.
